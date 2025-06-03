dd2c6b25a1c2e4268150e1dcea8c14854498d22b
ElloRabyndra <Ellorabyndra123@gmail.com>
2025-06-03 09:25:16 +0700

    delete components

--- src/Components/Routing/Page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Page({ children }) {
-  return (
-    <div className="flex items-center justify-center w-full font-semibold shadow-xs h-60 bg-accent text-accent-foreground rounded-2xl">
-      {children}
-    </div>
-  );
-}
